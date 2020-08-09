1a7dd32840aafe8cac4d4d1cd6f70c37756841a9
Downy21 <cikinangka007@gmail.com>
2020-08-10 02:22:13 +0700

    Penambahan Materi OOP toSrtring 2 :)

--- src/app/toString.kt
+++ src/app/toString.kt
@@ -6,11 +6,13 @@ fun main() {
 
     val pengguna1 = User("DoniCeriaCelalu" , "22455")
     val pengguna2 = User("DitaCeliacelalu","44565")
+    val pengguna3 = User("Rahmat","22345")
 
 
 
     println(pengguna1)
     println(pengguna2)
+    println(pengguna3)
 
 
 
