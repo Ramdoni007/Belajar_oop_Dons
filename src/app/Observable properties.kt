1aac4b5e7eaacba842462c7482c5fc37a009bc1b
Ramdoni007 <ramd42156@gmail.com>
2020-08-13 17:06:51 +0700

    Penambahan Materi observable properties : )

---
+++ src/app/Observable properties.kt
@@ -0,0 +1,16 @@
+package app
+
+import data.Account
+
+fun main() {
+
+    val account =  Account()
+
+    account.description = "Doni Ganteng"
+    account.description ="Deskripsi nya Jadi Milea Istri Doni"
+
+
+
+
+
+}
