72247afd68d4f75119222c713d5620f856eaa7be
Ramdoni007 <ramd42156@gmail.com>
2020-08-13 10:53:51 +0700

    Penambahan Materi Inline Class  : )

---
+++ src/app/Inline Class.kt
@@ -0,0 +1,14 @@
+package app
+
+import data.MerkSabun
+
+fun main() {
+
+    val merk = MerkSabun("Shinzui")
+    println(merk.value)
+    println(merk.toUpper("lifebouy"))
+
+
+
+
+}
---
+++ src/data/MerkSabun.kt
@@ -0,0 +1,9 @@
+package data
+
+inline class MerkSabun (val value: String ) {
+
+    fun toUpper(name: String ) : String {
+        return name.toUpperCase()
+
+    }
+}
